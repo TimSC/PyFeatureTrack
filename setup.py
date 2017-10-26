e36132adea4aa3f48e321e8b76d4fa6bd52cd7fa
Mohab Emad Mohammed Meshref <mohab1989@users.noreply.github.com>
2017-10-26 14:53:33 +0200

    Update setup.py
    importing numpy and adding it to include directories to fix setup errors due to not finding numpy

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("goodFeaturesUtils", ["goodFeaturesUtils.pyx"]),
 	Extension("trackFeaturesUtils", ["trackFeaturesUtils.pyx"])]
+    include_dirs=[np.get_include()]
 )
 
